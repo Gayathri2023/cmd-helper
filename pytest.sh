2daaf53f2ab282e3d8bb4de7c7362254943fe9c5
Gayathri2023 <123184201+Gayathri2023@users.noreply.github.com>
2026-04-02 20:46:16 +0530

    Update pytest.sh

--- pytest.sh
+++ pytest.sh
@@ -1,4 +1,28 @@
+# docs
+# https://docs.pytest.org/en/stable/getting-started.html
+
+mkdir project_name
+cd project_name
+
 python3 -m venv .venv
 source .venv/bin/activate
+
+# optional
 curl -L -O https://raw.githubusercontent.com/github/gitignore/refs/heads/main/Python.gitignore
 mv Python.gitignore .gitignore
+
+pip install -U pytest
+pytest --version
+
+mkdir tests
+cd tests
+touch test_sample.py
+
+# content of test_sample.py
+def func(x):
+    return x + 1
+    
+def test_answer():
+    assert func(3) == 5
+
+pytest
