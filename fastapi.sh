8a3fc8908ed8e835927b0924e2e1f751c8b484da
Gayathri2023 <123184201+Gayathri2023@users.noreply.github.com>
2026-04-02 21:38:23 +0530

    Update fastapi.sh

--- fastapi.sh
+++ fastapi.sh
@@ -1,3 +1,17 @@
 # docs
 # https://fastapi.tiangolo.com/
 
+mkdir project_name
+cd project_name
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+# optional
+curl -L -O https://raw.githubusercontent.com/github/gitignore/refs/heads/main/Python.gitignore
+mv Python.gitignore .gitignore
+
+pip install "fastapi[standard]"
+
+touch main.py
+uvicorn main:app --host 127.0.0.1 --port 1234
