b299523d209d6037dc5f7e8ff64e457f131fa08d
Gayathri2023 <123184201+Gayathri2023@users.noreply.github.com>
2026-04-06 11:32:31 +0530

    Create test_db_override.py

---
+++ test_db_override.py
@@ -0,0 +1,42 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from main import app, get_db
+from database import Base
+
+# Test DB (temporary SQLite)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+# Create tables in test DB
+Base.metadata.create_all(bind=engine)
+
+# Override dependency
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Apply override
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
+
+def test_create_user():
+    response = client.post("/users", json={"name": "Gayathri", "job": "QA"})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "Gayathri"
